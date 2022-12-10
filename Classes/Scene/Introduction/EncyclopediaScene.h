3699b5bbd898905a379c7c5056cd335f02701d95
Leo <liaocs2008@users.noreply.github.com>
2022-12-10 09:53:17 -0800

    visual studio compiler can't parse these unknown characters in comments, and they will fail the compilation.

--- Classes/Scene/Introduction/EncyclopediaScene.h
+++ Classes/Scene/Introduction/EncyclopediaScene.h
@@ -8,10 +8,10 @@ public:
     static cocos2d::Scene* createScene();
 	bool init() override;
 
-	void menuNextCallback1(Ref *pSender );//����ٿ���ʾ��ť�������л�
-	void menuNextCallback2(Ref *pSender );//����ٿƷ�������ť�������л�
-	void menuNextCallback3(Ref *pSender );//����ٿƵ��˰�ť�������л�
-	void menuNextCallback4(Ref *pSender );//����ٿƹرհ�ť�������л�
+	void menuNextCallback1(Ref *pSender );
+	void menuNextCallback2(Ref *pSender );
+	void menuNextCallback3(Ref *pSender );
+	void menuNextCallback4(Ref *pSender );
 
     CREATE_FUNC(EncyclopediaScene);
  
